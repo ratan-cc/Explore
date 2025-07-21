3a108d017111cfb8a2c042e580abb60d782e092c
Ratan kumar singh <anmolratan858@gmail.com>
2025-07-21 18:23:20 +0530

    New program

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int c =49;
+        System.out.println("Ratan Kumar Singh");
+        System.out.println("Galgotias...");
+        System.out.println(c);
+
+    }
+}
